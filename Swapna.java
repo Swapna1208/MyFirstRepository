0080ead1e473acffb523f361770b41a6a51eed39
Swapna1208 <41814711+Swapna1208@users.noreply.github.com>
2018-12-19 10:47:15 +0530

    Update Swapna.java

--- Swapna.java
+++ Swapna.java
@@ -1,5 +1,10 @@
 class Swapna{
+  String s;
+  Swapna(String s){
+    this.s;
+  }
 public static void main(String[] args){
+  Swapna obj= new Swapna(Swapna);
 System.out.println("Hello world");
 }
 }
